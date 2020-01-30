5df11fbd48c056a12e18367b5086493ef6845892
Vincent Villatte <vincent.villatte@lifeautomation.com.au>
2020-01-30 22:52:23 +1100

    Added debug info to setup and loop

--- src/RoboCar.ino
+++ src/RoboCar.ino
@@ -6,10 +6,12 @@ byte status = 0;
 void setup()
 {
 	status = myRoboCar.init();
+    Serial.println("RoboCar::setup()");
     if(0 != status) Serial.println("Error initialising myRoboCar");
 }
 
 void loop()
 {
+    Serial.println("RoboCar::loop()");
     myRoboCar.run();
 }
